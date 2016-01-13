7e1c9cc7cae5f0a4cd1a2dbd6428f6eca7799864
stang23 <shaofeng.tang@intel.com>
2016-01-13 11:29:54 +0800

    enable Android build

--- tools/install_android_ndk.sh
+++ tools/install_android_ndk.sh
@@ -4,6 +4,6 @@ wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
 
 chmod a+x android-ndk-r10e-linux-x86_64.bin
  
-./android-ndk-r10e-linux-x86_64.bin
+./android-ndk-r10e-linux-x86_64.bin >/dev/null 2>&1
 
 mv android-ndk-r10e ../android-ndk 
