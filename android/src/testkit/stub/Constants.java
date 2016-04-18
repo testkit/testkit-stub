5869064a37159dc2da27fff5ca1ef1dd944b6e2d
stang23 <shaofeng.tang@intel.com>
2016-04-18 20:03:21 +0800

    update the contents

--- android/src/testkit/stub/Constants.java
+++ android/src/testkit/stub/Constants.java
@@ -2,12 +2,12 @@ package testkit.stub;
 
 public class Constants {
     public interface ACTION {
-        public static String MAIN_ACTION = "com.truiton.foregroundservice.action.main";
-        public static String PREV_ACTION = "com.truiton.foregroundservice.action.prev";
-        public static String PLAY_ACTION = "com.truiton.foregroundservice.action.play";
-        public static String NEXT_ACTION = "com.truiton.foregroundservice.action.next";
-        public static String STARTFOREGROUND_ACTION = "com.truiton.foregroundservice.action.startforeground";
-        public static String STOPFOREGROUND_ACTION = "com.truiton.foregroundservice.action.stopforeground";
+        public static String MAIN_ACTION = "com.intel.testkit.action.main";
+        public static String PREV_ACTION = "com.intel.testkit.action.prev";
+        public static String PLAY_ACTION = "com.intel.testkit.action.play";
+        public static String NEXT_ACTION = "com.intel.testkit.action.next";
+        public static String STARTFOREGROUND_ACTION = "com.intel.testkit.action.startforeground";
+        public static String STOPFOREGROUND_ACTION = "com.intel.testkit.action.stopforeground";
     }
 
     public interface NOTIFICATION_ID {
